1af7cd43786b1691907264b63b81d8b1c5a6a9ba
Xumoyun23 <IT1@kafil.head>
2021-11-25 12:53:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
+
+
+themeSwitch.onclick = function(){
+    themeSwitch.classList.toggle('active');
+    body.classList.toggle('dark');
+}
+
+let menutoggle = document.querySelector('.toggle')
+let navigation = document.querySelector('.navigation')
+
+toggle.onclick = function(){
+    menutoggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
